4cef8d3dca45b1acd7d27540aa021291e870efc3
kalicharan-practice <kalicharan.practice@gmail.com>
2026-05-29 12:37:36 +0530

    terraform vpc eks iam setup again

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,34 @@
+module "vpc_core" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs = ["us-east-1a", "us-east-1b"]
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+}
+# module "vpc_core" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.8.1"
+
+#   name = var.vpc_name
+#   cidr = var.vpc_cidr
+
+#   azs = ["us-east-1a", "us-east-1b"]
+
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
+
+#   enable_dns_hostnames = true
+# }
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -9,3 +9,15 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.vpc_core.private_subnets
 }
+
+# output "vpc_id" {
+#   value = module.vpc_core.vpc_id
+# }
+
+# output "public_subnets" {
+#   value = module.vpc_core.public_subnets
+# }
+
+# output "private_subnets" {
+#   value = module.vpc_core.private_subnets
+# }
